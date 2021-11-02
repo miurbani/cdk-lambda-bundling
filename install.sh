98658ad132d9b732820f5871ac598cda5ae5c248
Michal Urbaniak <miurbani@amazon.com>
2021-11-02 14:26:53 +0800

    Updating installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo yum install maven
+sudo sh resize.sh
+sudo yum install -y maven
 sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
-sudo yum install dotnet-sdk-3.1
+sudo yum install -y dotnet-sdk-3.1
+sudo dotnet tool install -g Amazon.Lambda.Tools
+exec bash -l
